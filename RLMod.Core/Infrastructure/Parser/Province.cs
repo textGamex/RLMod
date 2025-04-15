c7e43d675f4e1b1c1da31a73b8b1cd79a2fd1c56
XYY1411 <1411483638@qq.com>
2025-04-15 20:32:58 +0800

    feat: add System.Drawing.Common package and add province-parser

---
+++ RLMod.Core/Infrastructure/Parser/Province.cs
@@ -0,0 +1,12 @@
+﻿namespace RLMod.Core.Infrastructure.Parser;
+
+public class Province
+{
+    public int Id { get; set; }
+    public Rgb Color { get; set; }
+    public string? ProvinceType { get; set; }
+    public bool? IsCoastal { get; set; }
+    public string? Terrain { get; set; }
+    public int? ContinentId { get; set; }
+    public HashSet<int> Adjacencies { get; set; } = [];
+}
---
+++ RLMod.Core/Infrastructure/Parser/ProvinceParser.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.Json;
+
+namespace RLMod.Core.Infrastructure.Parser;
+
+public class ProvinceParser
+{
+    public static void DoParser()
+    {
+        Console.WriteLine("Starting province parser...");
+
+        string provincesFilePath = @"D:\SteamLibrary\steamapps\common\Hearts of Iron IV\map\provinces.bmp";
+        string definitionFilePath = @"D:\SteamLibrary\steamapps\common\Hearts of Iron IV\map\definition.csv";
+        string provincesJsonFilePath = @"D:\Worktable\hoi4_map_reader\State_reader\out\provinces.json";
+        Console.WriteLine($"provincesFilePath: {provincesFilePath}");
+        Console.WriteLine($"definitionFilePath: {definitionFilePath}");
+        Console.WriteLine($"provincesJsonFilePath: {provincesJsonFilePath}");
+        string[] csvLines = File.ReadAllLines(definitionFilePath);
+        SortedList<int, Province> provinces = [];
+        Dictionary<Rgb, int> colorToProvinceId = [];
+
+        for (int i = 1; i < csvLines.Length; i++)
+        {
+            string[] csvFields = csvLines[i].Split(';');
+            int provinceId = int.Parse(csvFields[0]);
+            Rgb color = new(byte.Parse(csvFields[1]), byte.Parse(csvFields[2]), byte.Parse(csvFields[3]));
+
+            Province province = new()
+            {
+                Id = provinceId,
+                Color = color,
+                ProvinceType = csvFields[4],
+                IsCoastal = bool.Parse(csvFields[5]),
+                Terrain = csvFields[6],
+                ContinentId = int.Parse(csvFields[7]),
+            };
+
+            provinces[provinceId] = province;
+            colorToProvinceId[color] = provinceId;
+        }
+
+        ConcurrentBag<(int provinceA, int provinceB)> edgePairs = [];
+
+        using (Bitmap bmpProvinces = new(provincesFilePath))
+        {
+            int width = bmpProvinces.Width;
+            int height = bmpProvinces.Height;
+            Rectangle rect = new(0, 0, width, height);
+            var data = bmpProvinces.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+            unsafe
+            {
+                byte* scan0 = (byte*)data.Scan0;
+                int stride = data.Stride;
+
+                Parallel.For(
+                    0,
+                    height,
+                    i =>
+                    {
+                        byte* rowPtr = scan0 + i * stride;
+                        for (int j = 0; j < width; j++)
+                        {
+                            byte* pixelPtr = rowPtr + j * 3;
+
+                            Rgb currentColor = new(pixelPtr[2], pixelPtr[1], pixelPtr[0]);
+                            int currentProvinceId = colorToProvinceId[currentColor];
+
+                            if (j < width - 1)
+                            {
+                                byte* rightPtr = pixelPtr + 3;
+                                Rgb rightColor = new(rightPtr[2], rightPtr[1], rightPtr[0]);
+                                int rightProvinceId = colorToProvinceId[rightColor];
+                                if (currentProvinceId != rightProvinceId)
+                                {
+                                    edgePairs.Add((currentProvinceId, rightProvinceId));
+                                }
+                            }
+
+                            if (i < height - 1)
+                            {
+                                byte* downPtr = scan0 + (i + 1) * stride + j * 3;
+                                Rgb downColor = new(downPtr[2], downPtr[1], downPtr[0]);
+                                int downProvinceId = colorToProvinceId[downColor];
+                                if (currentProvinceId != downProvinceId)
+                                {
+                                    edgePairs.Add((currentProvinceId, downProvinceId));
+                                }
+                            }
+                        }
+                    }
+                );
+            }
+            bmpProvinces.UnlockBits(data);
+        }
+
+        foreach (var (provinceA, provinceB) in edgePairs)
+        {
+            provinces[provinceA].Adjacencies.Add(provinceB);
+            provinces[provinceB].Adjacencies.Add(provinceA);
+        }
+
+        string jsonString = JsonSerializer.Serialize(
+            provinces,
+            new JsonSerializerOptions { WriteIndented = true }
+        );
+        File.WriteAllText(provincesJsonFilePath, jsonString);
+        Console.WriteLine("Provinces data has been written to " + provincesJsonFilePath);
+    }
+}
