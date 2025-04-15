2be24b5728d60db074b8bb57b6023b10e4afaef7
XYY1411 <1411483638@qq.com>
2025-04-15 20:32:58 +0800

    feat: add System.Drawing.Common package and add province-parser

---
+++ RLMod.Core/Infrastructure/Parser/Rgb.cs
@@ -0,0 +1,18 @@
+﻿namespace RLMod.Core.Infrastructure.Parser;
+
+public readonly struct Rgb(byte r, byte g, byte b) : IEquatable<Rgb>
+{
+    private byte R { get; } = r;
+    private byte G { get; } = g;
+    private byte B { get; } = b;
+
+    public static bool operator ==(Rgb a, Rgb b) => a.R == b.R && a.G == b.G && a.B == b.B;
+
+    public static bool operator !=(Rgb a, Rgb b) => !(a == b);
+
+    public bool Equals(Rgb other) => R == other.R && G == other.G && B == other.B;
+
+    public override readonly bool Equals(object? obj) => obj is Rgb other && Equals(other);
+
+    public override readonly int GetHashCode() => HashCode.Combine(R, G, B);
+}
