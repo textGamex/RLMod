20b81ac058d51d63eb1a5ebd0e2e052c9b9d4b9d
textGamex <textGamex@outlook.com>
2025-04-15 12:36:53 +0800

    添加项目文件。

---
+++ RLMod.Core/App.xaml.cs
@@ -0,0 +1,59 @@
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace RLMod.Core;
+
+public partial class App : Application
+{
+    public IServiceProvider Services => _host.Services;
+    public static new App Current => (App)Application.Current;
+
+    private readonly IHost _host;
+
+    public App()
+    {
+        InitializeComponent();
+
+        _host = CreateHost();
+    }
+
+    private static IHost CreateHost()
+    {
+        var settings = new HostApplicationBuilderSettings
+        {
+            Args = Environment.GetCommandLineArgs(),
+            ApplicationName = "VModer",
+            ContentRootPath = AppContext.BaseDirectory
+        };
+#if DEBUG
+        settings.EnvironmentName = "Development";
+#else
+        settings.EnvironmentName = "Production";
+#endif
+        var builder = Host.CreateApplicationBuilder(settings);
+        builder.Services.AddSingleton<MainWindow>();
+
+        // 添加 NLog 日志
+        builder.Logging.ClearProviders();
+        builder.Logging.AddNLog();
+        LogManager.Configuration = new NLogLoggingConfiguration(builder.Configuration.GetSection("NLog"));
+        return builder.Build();
+    }
+
+    private async void App_OnStartup(object sender, StartupEventArgs e)
+    {
+        await _host.StartAsync();
+        var mainWindow = Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+        _host.StopAsync();
+    }
+}
---
+++ RLMod.Core/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace RLMod.Core;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
