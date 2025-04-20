4ecc68be1479c164df4efb761389acda40468f51
textGamex <textGamex@outlook.com>
2025-04-21 01:53:03 +0800

    feat: 统一全局的随机数发生器的种子设置

--- RLMod.Core/Helpers/RandomHelper.cs
+++ RLMod.Core/Helpers/RandomHelper.cs
@@ -6,6 +6,11 @@ namespace RLMod.Core.Helpers;
 
 public static class RandomHelper
 {
+    /// <summary>
+    /// 获取一个随机数生成器, 如果设置了 <see cref="AppSettingService.RandomSeed"/>, 则使用该种子, 否则使用默认的随机种子.
+    /// </summary>
+    /// <param name="threadSafe">是否是线程安全的</param>
+    /// <returns>一个随机数生成器</returns>
     public static MersenneTwister GetRandomWithSeed(bool threadSafe = false)
     {
         var settingService = App.Current.Services.GetRequiredService<AppSettingService>();
