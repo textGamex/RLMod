1cadd1ed862d5d24172592042ce788758bd20245
textGamex <textGamex@outlook.com>
2025-04-23 13:05:28 +0800

    perf: 优化Rgb结构性能

--- RLMod.Core/Infrastructure/Parser/Rgb.cs
+++ RLMod.Core/Infrastructure/Parser/Rgb.cs
@@ -2,17 +2,17 @@
 
 public readonly struct Rgb(byte r, byte g, byte b) : IEquatable<Rgb>
 {
-    private byte R { get; } = r;
-    private byte G { get; } = g;
-    private byte B { get; } = b;
+    private readonly byte _r = r;
+    private readonly byte _g = g;
+    private readonly byte _b = b;
 
-    public static bool operator ==(Rgb a, Rgb b) => a.R == b.R && a.G == b.G && a.B == b.B;
+    public static bool operator ==(Rgb a, Rgb b) => a._r == b._r && a._g == b._g && a._b == b._b;
 
     public static bool operator !=(Rgb a, Rgb b) => !(a == b);
 
-    public bool Equals(Rgb other) => R == other.R && G == other.G && B == other.B;
+    public bool Equals(Rgb other) => _r == other._r && _g == other._g && _b == other._b;
 
     public override bool Equals(object? obj) => obj is Rgb other && Equals(other);
 
-    public override int GetHashCode() => HashCode.Combine(R, G, B);
+    public override int GetHashCode() => _r * 31 + _g * 31 + _b * 31;
 }
